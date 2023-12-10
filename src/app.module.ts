81d58266fa6f12d440f20722690d0a9959b8ef93
Wojciech Marek <marek.wojciech@outlook.com>
2023-12-10 18:04:52 +0100

    feat: add services module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ApiModule } from './api/api.module';
 import { DomainModule } from './domain/domain.module';
+import { ServicesModule } from './services/services.module';
 
 @Module({
-  imports: [ApiModule, DomainModule],
+  imports: [ApiModule, DomainModule, ServicesModule],
   controllers: [],
   providers: [],
 })

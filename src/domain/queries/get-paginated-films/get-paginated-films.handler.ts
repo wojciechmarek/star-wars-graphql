8d9a63fa88899946cb9fe9749b28772be4bd458c
Wojciech Marek <marek.wojciech@outlook.com>
2023-12-10 20:18:23 +0100

    feat: add cache to rest of film query handlers

--- src/domain/queries/get-paginated-films/get-paginated-films.handler.ts
+++ src/domain/queries/get-paginated-films/get-paginated-films.handler.ts
@@ -1,15 +1,24 @@
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
 import { GetPaginatedFilmsQuery } from './get-paginated-films.query';
 import { FilmApiService } from 'src/services/external-api';
+import { Film } from 'src/models';
+import { CacheService } from 'src/services/persistence';
 
 @QueryHandler(GetPaginatedFilmsQuery)
 export class GetPaginatedFilmsQueryHandler
   implements IQueryHandler<GetPaginatedFilmsQuery>
 {
-  constructor(private readonly filmApiService: FilmApiService) {}
+  constructor(
+    private readonly filmApiService: FilmApiService,
+    private readonly cacheService: CacheService,
+  ) {}
 
-  async execute(query: GetPaginatedFilmsQuery) {
+  async execute(query: GetPaginatedFilmsQuery): Promise<Film[]> {
     const { page } = query;
-    return this.filmApiService.getPaginatedFilms(page);
+    const cachedFilms = await this.cacheService.manageCache<Film[]>(
+      `GetPaginatedFilmsQuery-${page}`,
+      () => this.filmApiService.getPaginatedFilms(page),
+    );
+    return cachedFilms;
   }
 }
--- src/domain/queries/get-unique-words/get-unique-words.handler.ts
+++ src/domain/queries/get-unique-words/get-unique-words.handler.ts
@@ -1,12 +1,23 @@
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
 import { GetUniqueWordsQuery } from './get-unique-words.query';
+import { FilmApiService } from 'src/services/external-api';
+import { CacheService } from 'src/services/persistence';
+import { Film } from 'src/models';
 
 @QueryHandler(GetUniqueWordsQuery)
 export class GetUniqueWordsQueryHandler
   implements IQueryHandler<GetUniqueWordsQuery>
 {
-  constructor() {}
-  async execute(query) {
-    return [];
+  constructor(
+    private readonly filmApiService: FilmApiService,
+    private readonly cacheService: CacheService,
+  ) {}
+  async execute() {
+    const cachedAllFilms = await this.cacheService.manageCache<Film[]>(
+      `GetUniqueWordsQuery`,
+      () => this.filmApiService.getAllFilms(),
+    );
+
+    return cachedAllFilms;
   }
 }
